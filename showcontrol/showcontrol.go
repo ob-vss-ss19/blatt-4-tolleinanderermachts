4ebcd0441458e8503c0d53532208d62009209226
Andreas Stiglmeier <stiglmeier.andreas@gmail.com>
2019-06-24 14:23:19 +0200

    update showcontrol for consistency

--- showcontrol/showcontrol.go
+++ showcontrol/showcontrol.go
@@ -90,3 +90,41 @@ func (ctrl *ShowControl) CheckSeat(ctx context.Context,
 	}
 	return nil
 }
+
+func (ctrl *ShowControl) NotifyMovieDelete(ctx context.Context,
+	req *proto.MovieData, rsp *proto.RequestResponse) error {
+	println("got movie delete notification")
+	var delShows []int
+	for k, v := range ctrl.Shows {
+		if v.Movie == int(req.Id) {
+			delShows = append(delShows, int(k))
+			delete(ctrl.Shows, k)
+		}
+	}
+
+	go ctrl.notyfiyReservationcontrol(delShows)
+
+	rsp.Succeeded = true
+	return nil
+}
+
+func (ctrl *ShowControl) NotifyRoomDelete(ctx context.Context,
+	req *proto.RoomData, rsp *proto.RequestResponse) error {
+	println("got room delete notification")
+	var delShows []int
+	for k, v := range ctrl.Shows {
+		if v.Room == int(req.Id) {
+			delShows = append(delShows, int(k))
+			delete(ctrl.Shows, k)
+		}
+	}
+
+	go ctrl.notyfiyReservationcontrol(delShows)
+
+	rsp.Succeeded = true
+	return nil
+}
+
+func (ctrl *ShowControl) notyfiyReservationcontrol(delShows []int) {
+	//TODO: notify reservationcontrol that shows with ids in array delShows are deleted
+}
